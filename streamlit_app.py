b6c0357e8d85f9456d52f5a28a11e9746a58de17
Shubro <83620026+Shubro@users.noreply.github.com>
2023-01-09 12:11:52 +1100

    Update streamlit_app.py
    added additional headers and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Quinoa Bowl')
+streamlit.text('Chicken salad')
+streamlit.text('Chickpea and corn fritters')
