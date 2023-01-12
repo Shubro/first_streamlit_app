4e804a3af6efdc20f2fadd126994717558a83109
Shubro <83620026+Shubro@users.noreply.github.com>
2023-01-12 15:46:22 +1100

    Update streamlit_app.py
    api response in a pandas data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# put json data in a pandas data frame 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display the table on the page.
+streamlit.dataframe(fruityvice_normalized)
